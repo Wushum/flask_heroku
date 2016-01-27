0e340c4b70cf6a07bbe9fd48f9cb53d6407ae26e
Winston Lindsay <wnlindsay@yahoo.com>
2016-01-27 23:43:36 +0000

    customized app

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def add_header(response):
     return response
 
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
@@ -59,4 +60,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8888")
+    app.run(debug=True,host="0.0.0.0",port=8080)
